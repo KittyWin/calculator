a8738900d0f0a4eb00b9d08c14399b309e9d8d40
KittyWin <121171801+KittyWin@users.noreply.github.com>
2023-01-27 20:30:06 +0500

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,12 @@
+const a = +prompt ("Введите первое число");
+const b = +prompt ("Введите второе число");
+
+const c = a + b;
+const d = a * b;
+const h = a / b;
+const q = a - b;
+
+console.log( a + '+' + b + "=" + c);
+console.log( a + '*' + b + "=" + d);
+console.log( a + '/' + b + "=" + h);
+console.log( a + '-' + b + "=" + q);
